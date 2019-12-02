dff600c630fbefb295e788f1ef0d0b19ab8061e1
Claire Germ <clairegerm@live.nl>
2019-12-02 20:08:47 +0100

    Add files via upload

---
+++ model.py
@@ -0,0 +1,25 @@
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Conv2D, BatchNormalization, Activation
+
+def model(loss='mse', optimizer='adam'):
+    model = Sequential()
+    #Nvidia model
+    #Normalization and change of the input shape:
+    model.add(Lambda(lambda x:  (x / 127.5) - 1., input_shape=(70, 160, 3)))
+    #Add 5 Convolutional Layers
+    model.add(Conv2D(filters=24, kernel_size=5, strides=(2, 2), activation='relu'))
+    model.add(Conv2D(filters=36, kernel_size=5, strides=(2, 2), activation='relu'))
+    model.add(Conv2D(filters=48, kernel_size=5, strides=(2, 2), activation='relu'))
+    model.add(Conv2D(filters=64, kernel_size=3, strides=(1, 1), activation='relu'))
+    model.add(Conv2D(filters=64, kernel_size=3, strides=(1, 1), activation='relu'))
+
+    #Flatten Layers
+    model.add(Flatten())
+    #3 fully connected layers, 1 output layer
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(1))
+    model.compile(loss=loss, optimizer=optimizer)
+
+    return model
